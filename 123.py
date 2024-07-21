15372ed6313dbc0006378baf6e4e13573a373365
CreatorGodX <godofcreation980316@gmail.com>
2024-07-21 17:14:27 +0800

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,13 @@
+print(100)
+z=100
+print (z) 
+z=900
+print (z)
+age = 9000
+name = 'creator god'
+pen = 3.2
+rain = True
+print (age, name, pen, rain)
+name = input('type your name, please : ')
+d = 'hi'
+print (d, name, '!')
